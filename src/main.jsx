e96b655e9e7cffe0929d21d7243160925ccd2d3b
Spandan2405 <spandan2405@gmail.com>
2025-08-07 12:32:43 +0530

    Added Vercel analytics & favicon

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,15 @@ import App from "./App";
 import "./index.css";
 import { SpotifyProvider } from "./context/SpotifyContext";
 import { BrowserRouter as Router } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <SpotifyProvider>
-    <Router>
-      <App />
-    </Router>
-  </SpotifyProvider>
+  <>
+    <SpotifyProvider>
+      <Router>
+        <App />
+      </Router>
+    </SpotifyProvider>
+    <Analytics />
+  </>
 );
